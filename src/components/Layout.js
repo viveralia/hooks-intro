da7abc054a5e3508d272002f644efbabbdda2932
viveralia <edgarv10@gmail.com>
2019-11-04 18:18:08 -0600

    Alignment bug fixed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ const Layout = ({ children }) => {
   return (
     <div className='container my-4'>
       <div className='row'>
-        <div className='col-12 col-md-6 mx auto'>{children}</div>
+        <div className='col-12 col-md-6 mx-auto'>{children}</div>
       </div>
     </div>
   )
